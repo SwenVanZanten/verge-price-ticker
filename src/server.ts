b4fdbe1e5b0579b39622b7211ac283b04857f251
Swen van Zanten <info@hdsign.nl>
2019-11-03 18:30:48 +0100

    Add cross domain support

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,15 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
+const allowCrossDomain = (req: any, res: any, next: any) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
 // middleware to parse input
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
